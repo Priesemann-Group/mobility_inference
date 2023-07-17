02249dc51571ad429ad7474571fcb33ac20db2e0
emilIftekhar <38589667+emilIftekhar@users.noreply.github.com>
2023-07-17 09:28:49 +0200

    complete model

---
+++ model.py
@@ -0,0 +1,112 @@
+import pymc as pm
+import numpy as np
+import pandas as pd
+
+# import matplotlib.pyplot as plt
+import arviz as az
+import pickle
+
+# from scipy import stats
+import pytensor.tensor as at
+import xarray as xr
+
+import covid19_inference.covid19_inference as cov19
+import utils
+
+# increase plt fontsize
+# plt.rcParams.update({'font.size': 20})
+# from matplotlib import lines, patches
+
+
+# Data
+## Out of home duration
+path_mobility = "data/mobility/mobilityData_OverviewBL_weekly.csv"
+mobility_df = pd.read_csv(path_mobility, parse_dates=True, index_col=0, delimiter=";")
+mobility_df = mobility_df[mobility_df["BundeslandID"] == "Deutschland"]
+mobility_df["week"] = mobility_df.index.isocalendar().week
+
+### get dates
+mobility_dates = mobility_df.index.values
+mobility_dates_2020 = mobility_dates[mobility_dates < np.datetime64("2020-12-20")]
+mobility_dates_2020 = mobility_dates_2020[
+    mobility_dates_2020 > np.datetime64("2020-03-29")
+]
+mobility_dates_2022 = mobility_dates[mobility_dates < np.datetime64("2022-12-20")]
+mobility_dates_2022_shortened = mobility_dates_2022[-len(mobility_dates_2020) :]
+
+### get out of home duration data
+mobility_df_2020 = mobility_df.filter(items=mobility_dates_2020, axis=0)
+mobility_data_2020 = mobility_df_2020["outOfHomeDuration"].to_xarray()
+
+baseline_mobility_df = pd.read_csv(
+    "data/mobility/baseline_mobility.csv", parse_dates=True, index_col=0
+)
+baseline_mobility_df = baseline_mobility_df.filter(
+    items=mobility_dates_2022_shortened, axis=0
+)
+## replace index by mobility_dates_2020
+baseline_mobility_df.index = mobility_dates_2020
+## rename index to "date"
+baseline_mobility_df.index.name = "date"
+baseline_mobility = baseline_mobility_df["outOfHomeDuration"].to_xarray()
+
+## R
+path_R = "data/R/Germany/R_eff_Germany.csv"
+R_df = pd.read_csv(path_R, index_col=1, parse_dates=True)
+
+### weekly average placed on Sunday
+df = utils.weekly_formatting(R_df, mobility_dates_2020)
+df = utils.transform_data(df["R_eff median"])
+R_data = df.to_xarray()
+
+## NPI
+stay_at_home_2020 = utils.get_NPI_data(
+    "data/NPIs/stay-at-home-covid.csv", mobility_dates_2020
+)
+stay_at_home_2020 = utils.transform_data(stay_at_home_2020)
+stay_at_home_2020 = stay_at_home_2020["stay_home_requirements"].to_xarray()
+
+## Temperature
+weather_df = pd.read_csv(
+    "data/weather/weatherData2020and2022.csv", parse_dates=True, index_col=0
+)
+
+### filter for mobility_dates_2020
+weather_df_2020 = weather_df[weather_df.index.isin(mobility_dates_2020)]
+weather_df_2022 = weather_df[weather_df.index.isin(mobility_dates_2022_shortened)]
+
+### calculate differences between the years
+delta_tmax = utils.return_differences(weather_df_2020, weather_df_2022, "tmax")
+delta_tmax = xr.DataArray(
+    delta_tmax, dims="date", coords={"date": mobility_dates_2020}, name="delta_tmax"
+)
+
+### calculate average between the years
+average_tmax = utils.return_averages(weather_df_2020, weather_df_2022, "tmax")
+average_tmax = xr.DataArray(
+    average_tmax, dims="date", coords={"date": mobility_dates_2020}, name="average_tmax"
+)
+
+# Model
+R_model = pm.Model()
+utils.single_indicator_model(
+    R_data,
+    R_model,
+    baseline_mobility,
+    mobility_data_2020,
+    stay_at_home_2020,
+    delta_tmax,
+    average_tmax,
+)
+
+# Inference
+R_trace = pm.sample(model=R_model, draws=100, tune=100, cores=1, chains=2)
+with R_model:
+    pm.compute_log_likelihood(R_trace)
+
+# Saving the results
+tag = "R_model_full"
+# Save the trace
+path = f"results/raw/trace_{tag}.pickle"
+with open(path, "wb") as inference_file:
+    pickle.dump(R_trace, inference_file)
--- utils.py
+++ utils.py
@@ -1,6 +1,10 @@
 import numpy as np
 import scipy.stats as stats
 import pandas as pd
+import pymc as pm
+import pytensor.tensor as at
+
+import covid19_inference.covid19_inference as cov19
 
 
 def _cut_off_before_monday(df):
@@ -27,3 +31,131 @@ def get_NPI_data(filename_in, dates_in):
     df = pd.read_csv(filename_in, index_col=2, parse_dates=True)
     df = df[df["Entity"] == "Germany"]
     return weekly_formatting(df, dates_in)
+
+
+# calculate differences between the years
+def return_differences(df2020_in, df2022_in, label_in):
+    delta = df2020_in[label_in].values - df2022_in[label_in].values
+    if label_in == "prcp":
+        delta = -delta
+    return delta
+
+
+# calculate average between the years
+def return_averages(df1_in, df2_in, label_in):
+    average = (df1_in[label_in].values + df2_in[label_in].values) / 2
+    return average
+
+
+# 'optimal' temperature
+def generate_Tstar(amplitude, scale, offset, shift, length):
+    x = at.linspace(0, length, length)
+    return amplitude * at.sin(at.cos(scale * x + shift)) + offset
+
+
+# function for modulating the impact of temperature
+def Gaussian(T, T_star):
+    return at.exp(-at.power(T - T_star, 2.0))
+
+
+# single indicator model
+def single_indicator_model(
+    data_in,
+    model_in,
+    base_mobility_data_in,
+    observed_mobility_data_in,
+    NPI_data_in,
+    del_weather_data_in,
+    avg_weather_data_in,
+    data_str_in="disease_data",
+    factor_str_in="z",
+    mu_str_in="mu",
+    sigma_str_in="sigma",
+    d_str_in="d",
+    m_str_in="m",
+):
+    Gaussian = np.vectorize(Gaussian)
+    len_data = data_in.shape[0]
+    with model_in:
+        # define data
+        m_base_data = pm.ConstantData("m_base", base_mobility_data_in)
+        disease_data = pm.ConstantData(data_str_in, data_in)
+        NPI_data = pm.ConstantData("NPI_data", NPI_data_in)
+        delta_weather = pm.ConstantData("delta_weather", del_weather_data_in)
+        avg_weather = pm.ConstantData("avg_weather", avg_weather_data_in)
+
+        # define hyper-parameters
+        mu_factor = 0.2
+        sigma_factor = 0.05
+        mu_mu = 1.0
+        sigma_mu = 0.2
+        mu_sigma = 0.3
+        sigma_sigma = 0.1
+
+        # meta-parameters for the convolution function (delay_cases)
+        model_in.diff_data_sim = (
+            0  # we are only interested in the reported cases, so no delay
+        )
+        model_in.sim_len = len_data - model_in.diff_data_sim
+
+        # impact of disease spread
+        ## define priors
+        factor_disease = pm.Normal(factor_str_in, mu=mu_factor, sigma=sigma_factor)
+        mu_disease = pm.LogNormal(mu_str_in, mu=mu_mu, sigma=sigma_mu)
+        sigma_disease = pm.LogNormal(sigma_str_in, mu=mu_sigma, sigma=sigma_sigma)
+        ## convolution
+        risk = cov19.model.delay_cases(
+            cases=disease_data,
+            delay_kernel="gamma",
+            median_delay=mu_disease,
+            scale_delay=sigma_disease,
+            len_input_arr=len_data,
+            len_output_arr=model_in.sim_len,
+        )
+        ## put it together
+        exponent = -factor_disease * risk
+        d = pm.Deterministic(d_str_in, at.exp(exponent))
+
+        # impact of NPI
+        ## define priors
+        factor_NPI = pm.Normal("z_NPI", mu=mu_factor, sigma=sigma_factor)
+        mu_NPI = pm.LogNormal("mu_NPI", mu=mu_mu, sigma=sigma_mu)
+        sigma_NPI = pm.LogNormal("sigma_NPI", mu=mu_sigma, sigma=sigma_sigma)
+        ## convolution
+        risk = cov19.model.delay_cases(
+            cases=NPI_data,
+            delay_kernel="gamma",
+            median_delay=mu_NPI,
+            scale_delay=sigma_NPI,
+            len_input_arr=len_data,
+            len_output_arr=model_in.sim_len,
+        )
+        ## put it together
+        exponent = -factor_NPI * risk
+        s = pm.Deterministic("s", at.exp(exponent))
+
+        # impact of weather: maximum temperature
+        factor_weather = pm.LogNormal("z_weather", mu=10, sigma=2)
+        ## Gaussian to model the impact of optimal absolute temperature
+        amplitude = pm.Normal("amplitude", mu=19, sigma=2)
+        scale = pm.Normal("scale", mu=0.07, sigma=0.01)
+        offset = pm.Normal("offset", mu=10, sigma=2)
+        shift = pm.Normal("shift", mu=5, sigma=1)
+        T_star = generate_Tstar(
+            amplitude=amplitude,
+            scale=scale,
+            offset=offset,
+            shift=shift,
+            length=len_data,
+        )
+        weather_relevance = Gaussian(avg_weather, T_star)
+        w = pm.Deterministic(
+            "W", at.exp(delta_weather * weather_relevance / factor_weather)
+        )
+
+        # define likelihood
+        m = pm.Deterministic(m_str_in, m_base_data * d * s * w)
+        model_error = pm.HalfCauchy("sigma_model", beta=0.5)
+        likelihood = pm.Normal(
+            "likelihood", mu=m, sigma=model_error, observed=observed_mobility_data_in
+        )
