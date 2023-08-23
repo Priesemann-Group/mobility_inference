a4dd8291ea56163e9c8234f42a2c8c3572166208
Emil Iftekhar <iftekharemil@yahoo.de>
2023-08-23 13:21:32 +0200

    disease_indicators_plot

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import plot
 import utils
 
 # Set up basic configurations
-name = "none"  # Name of the experiment
+name = "none_kernel_low_tau"  # Name of the experiment
 pandemic_fatigue = "none"  # Type of pandemic fatigue function: 'linear' or 'sigmoid'
 test = False  # Whether to run a test with fewer samples
 single = False  # Whether to run a single model
