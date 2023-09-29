36031dd918ab7a1c14d96d734d68cf6eab7151e0
Emil Iftekhar <iftekharemil@yahoo.de>
2023-09-29 14:54:35 +0200

    save ELPD components

--- model_comparison.py
+++ model_comparison.py
@@ -1,6 +1,7 @@
 import numpy as np
+import pandas as pd
 
-def approximate_probability_density(inference_model, trace, L, M, N_in, n_samples=500):
+def approximate_probability_density(inference_model, trace, i_in, M, N_in, dir_name_in, n_samples=500):
     # Calculate the log probability densities of the data given the inferred parameters
     log_likelihood_func_tmp = inference_model.compile_logp(vars=inference_model.free_RVs[:-1]+inference_model.observed_RVs, sum=False)
     ## Only get log p function for observed variables
@@ -13,30 +14,37 @@ def approximate_probability_density(inference_model, trace, L, M, N_in, n_sample
             relevant_vars = inference_model.continuous_value_vars[:-1]
             var_dict = {key: var for key, var in variables if key in map(str, relevant_vars)}
             log_likelihood.append(log_likelihood_func(var_dict))
-    # Take sum from L+1 to L+M per sample to get relevant logp density
+
+    # Take sum from i+1 to i+M per sample to get relevant logp density
     relevant_logp = []
     for ls in log_likelihood:
         sample = ls[0]
-        relevant_logp.append(np.sum(sample[L+1:L+M]))
+        relevant_logp.append(np.sum(sample[i_in:i_in+M]))
 
     # Take exp to get probability density per sample
     probabilities = np.exp(relevant_logp)
 
-    # Take average over samples
     return np.mean(probabilities)
 
+
 def calculate_ELPD_LFO(model_in, traces_in, L_in, M_in, N_in):
+    ELPDs = {}
     sum = 0
     # sum over log probability densities of all 'predictions'
     for i in range(L_in, N_in - M_in):
         trace = traces_in[i]
-        prob = approximate_probability_density(model_in, trace, L_in, M_in, N_in, n_samples=200)
-        sum += np.log(prob)
-    return sum
+        prob = approximate_probability_density(model_in, trace, i, M_in, N_in, n_samples=1000)
+        component = np.log(prob)
+        ELPDs[i] = component
+        sum += component
+
+    return sum, ELPDs
+
 
 def save_ELPD_LFO(inference_model, traces, L, M, N, dir_name):
-    ELPD = calculate_ELPD_LFO(inference_model, traces, L, M, N)
-    # save ELPD result to file
-    path = f"{dir_name}/ELPD.csv"
-    with open(path, "w") as f:
-        f.write(str(ELPD))
+    # save ELPD results to file
+    mean_ELPD, ELPD_components = calculate_ELPD_LFO(inference_model, traces, L, M, N)
+    ELPD_components["mean"] = mean_ELPD
+    ELPD_df = pd.DataFrame.from_dict(ELPD_components, orient="index")
+    ELPD_df.columns = ["ELPD_LFO"]
+    ELPD_df.to_csv(dir_name + "/ELPD_LFO.csv")
